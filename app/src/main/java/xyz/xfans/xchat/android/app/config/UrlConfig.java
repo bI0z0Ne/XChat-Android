b4cc0909c072c4e70480144e6ddc8c07f9f4bb2b
xfans <zhuleiv5@outlook.com>
2015-08-02 20:29:08 +0800

    add http async

--- app/src/main/java/xyz/xfans/xchat/android/app/config/UrlConfig.java
+++ app/src/main/java/xyz/xfans/xchat/android/app/config/UrlConfig.java
@@ -4,7 +4,7 @@ package xyz.xfans.xchat.android.app.config;
  * Created by zhu on 2015/7/30.
  */
 public class UrlConfig {
-    private static final String BASE_URL = "";
-    public static final String LOGIN = BASE_URL + "";
-    public static final String GETFIRENDS = BASE_URL + "";
+    private static final String BASE_URL = "http://localhost:8080/";
+    public static final String LOGIN = BASE_URL + "XChat/api/v1/user/login";
+    public static final String GETFIRENDS = BASE_URL + "XChat/api/v1/user/getallusers";
 }
